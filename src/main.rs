baacd6a4b3688c2dddf8e23cb395b61c61bd3aa8
Bob Sullivan <robertemmettsullivan@gmail.com>
2025-12-18 15:06:49 -0500

    Added function for getting day of christmas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+    let day_of_christmas: String = get_day_of_christmas(1);
+    println!("Day of Christmas: {day_of_christmas}");
     println!("Hello, world!");
+}
+
+fn get_day_of_christmas(day: i32) -> String {
+    let day_number: String = day.to_string();
+    return day_number;
 }
